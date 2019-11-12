2a775818bc5074b4d49f7d8b0b79de943279fc12
EnemyVayne <53927185+EnemyVayne@users.noreply.github.com>
2019-11-12 01:23:53 -0600

    Add files via upload

---
+++ GameObject.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//https://www.youtube.com/watch?v=0T1U0kbu1Sk
+/**
+ *
+ * @author kylef
+ */
+public abstract class GameObject
+{
+   protected ID id;
+   protected int speedX,speedY;
+   
+}
